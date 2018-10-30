affaee7f040ded92c162c78704a9ab669bcef354
tjgus95 <elliejeong95@gmail.com>
2018-10-30 14:27:29 +0000

    setup

---
+++ HouseAlarm-master/housealarm.c
@@ -0,0 +1,23 @@
+#include <wiringPi.h>
+#include <stdio.h>
+#include <time.h>
+#include "ifttt.h"
+
+void init_alarm();
+void toalarmoff();
+void toalarming();
+void toalarmed();
+void totriggered();
+void toalarmsounding();
+int main (int argc, char *argv[])
+{
+  init_alarm();
+  return 0 ;
+}
+void init_alarm(){
+
+
+
+}
+
+
